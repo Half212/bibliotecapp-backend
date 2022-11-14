e7812785b4fdf9c46ba9cc27f0499ebda6330c9c
Aquiles Leite <53454449+Half212@users.noreply.github.com>
2022-11-14 14:23:54 -0300

    ajuste

--- src/main/java/com/desafiotechlead/biblioteca/configs/WebConfig.java
+++ src/main/java/com/desafiotechlead/biblioteca/configs/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig {
         List<String> all = Arrays.asList("*");
 
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(Arrays.asList("https://biblioteca-app-techlead.herokuapp.com/",
+        corsConfiguration.setAllowedOrigins(Arrays.asList("https://biblioteca-app-junior.herokuapp.com/",
                 "http://localhost:4200","https://you.server.domain.com"));
         corsConfiguration.setAllowedHeaders(all);
         corsConfiguration.setExposedHeaders(all);
