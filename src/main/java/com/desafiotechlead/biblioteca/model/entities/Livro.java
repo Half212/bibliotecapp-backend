43c2af958bd6d970a9f2e42571d01b6ff957cdef
Aquiles Leite <53454449+Half212@users.noreply.github.com>
2022-11-15 17:57:50 -0300

    go

--- src/main/java/com/desafiotechlead/biblioteca/model/entities/Livro.java
+++ src/main/java/com/desafiotechlead/biblioteca/model/entities/Livro.java
@@ -1,17 +1,19 @@
 package com.desafiotechlead.biblioteca.model.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
-@Data
-@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+
 public class Livro {
 
     @Id
@@ -34,5 +36,18 @@ public class Livro {
     @PrePersist
     public void PrePersist(){
         setDataCadastro(LocalDate.now());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Livro livro = (Livro) o;
+        return id != null && Objects.equals(id, livro.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
     }
 }
